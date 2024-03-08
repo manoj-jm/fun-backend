77152892bd5b423d7562b8df3c9ec74d2e16f1aa
Manoj <143937739+manoj-jm@users.noreply.github.com>
2024-03-08 13:22:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/manoj',(req,res)=>{
+console.log("This is my first edit file/ commit in github ");
+});
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
 })
