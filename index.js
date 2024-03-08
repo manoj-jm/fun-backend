80c1cb309cbd1a529ba1b769c62a3705c65748d0
manoj <manojjmvr49@gmail.com>
2024-03-08 13:45:48 +0530

    semicolon added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
-})
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
